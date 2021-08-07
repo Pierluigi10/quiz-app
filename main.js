d36c87842aff20317d3bf2a48ec1620ff17b0dac
pierluigi <pierluigibaiano@gmail.com>
2021-08-07 17:01:17 +0200

    add ex of answer and build function oneQuestion to bring out one question at a time

--- main.js
+++ main.js
@@ -2,12 +2,78 @@ const container = document.querySelector(".container");
 const welcomeBox = document.querySelector(".welcome");
 const buttonStart = document.querySelector(".buttonStart");
 const questionParagraph = document.querySelector(".question");
-const answerContainer = document.querySelector(".answers");
+const answersContainer = document.querySelector(".answers");
 const answers = document.querySelectorAll("answers");
 const scoreSpan = document.querySelector("#score");
 const buttonNext = document.querySelector(".buttonNext");
 
+//Questions with answers
+const questions = [
+  {
+    question: "What is JavaScript?",
+    answers: [
+      { text: "der", isAnswer: false },
+      { text: "die", isAnswer: false },
+      { text: "das", isAnswer: true },
+    ],
+  },
+  {
+    question: "2What is JavaScript?",
+    answers: [
+      { text: "der", isAnswer: false },
+      { text: "die", isAnswer: false },
+      { text: "das", isAnswer: true },
+    ],
+  },
+  {
+    question: "3What is JavaScript?",
+    answers: [
+      { text: "der", isAnswer: false },
+      { text: "die", isAnswer: false },
+      { text: "das", isAnswer: true },
+    ],
+  },
+  {
+    question: "4What is JavaScript?",
+    answers: [
+      { text: "der", isAnswer: false },
+      { text: "die", isAnswer: false },
+      { text: "das", isAnswer: true },
+    ],
+  },
+  {
+    question: "5What is JavaScript?",
+    answers: [
+      { text: "der", isAnswer: false },
+      { text: "die", isAnswer: false },
+      { text: "das", isAnswer: true },
+    ],
+  },
+];
+
+//first question must be with index =0.
+let firstQuestion = 0;
+
+//button to change page (welcomeBox receives display none and cointainer display grid)
 buttonStart.addEventListener("click", () => {
   welcomeBox.style.display = "none";
   container.style.display = "grid";
+
+  //function to bring out one question at a time
+  oneQuestion(firstQuestion);
 });
+
+const oneQuestion = (index) => {
+  const question = questions[firstQuestion];
+  questionParagraph.textContent = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.classList.add("answerButton");
+    button.append(answer.text);
+    answersContainer.appendChild(button);
+    button.dataset.isCorrect = answer.isAnswer;
+    button.addEventListener("click", checkAnswer);
+  });
+};
+
+const checkAnswer = ()=> {}
