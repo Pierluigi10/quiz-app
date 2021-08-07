4c3dca6ca4f65adf89037e91ec8927dd51b90472
pierluigi <pierluigibaiano@gmail.com>
2021-08-07 14:18:51 +0200

    connect html class to main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const welcomeBox = document.querySelector(".welcome");
+const buttonStart = document.querySelector(".buttonStart");
+const questionParagraph = document.querySelector(".question");
+const answerContainer = document.querySelector(".answers");
+const answers = document.querySelectorAll("answers");
+const scoreSpan = document.querySelector("#score");
+const buttonNext = document.querySelector(".buttonNext");
+
+
